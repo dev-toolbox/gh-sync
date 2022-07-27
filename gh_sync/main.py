453fead9113d0804606f42460622503949f439c0
Marc Ende <me@e-beyond.de>
2022-07-27 09:11:36 +0200

    Refactored

--- gh_sync/main.py
+++ gh_sync/main.py
@@ -7,37 +7,37 @@ base_path = "/Volumes/Daten/Projekte/Webs"
 logging.basicConfig(level=logging.INFO)
 
 
-def createDirectory(org):
+def create_dir(org):
     path = f"{base_path}/{org}"
     exists = os.path.exists(path)
     if not exists:
         logging.info(f"creating path {path}")
         os.makedirs(path)
 
-def get_projects(org, repo):
-    path = f"{base_path}/{org}/{repo.name}"
+
+def get_projects(org, repository):
+    path = f"{base_path}/{org}/{repository.name}"
     exists = os.path.exists(path)
     if not exists:
         logging.info(f"cloning repo  {path}")
-        Repo.clone_from(repo.clone_url, path)
+        Repo.clone_from(repository.clone_url, path)
     else:
-        r = Repo(path)
-        if not r.bare:
-            o = r.remotes.origin
-            if len(r.refs) > 0:
+        existing_repo = Repo(path)
+        if not existing_repo.bare:
+            remote_origin = existing_repo.remotes.origin
+            if len(existing_repo.refs) > 0:
                 logging.info(f"updating repo  {path}")
-                o.pull()
+                remote_origin.pull()
 
-g = Github("access_token")
 
 g = Github(login_or_token=os.getenv('GITHUB_TOKEN'))
-# Then play with your Github objects:
+# Then play with your GitHub objects:
 for repo in g.get_user().get_repos():
     if repo.organization is not None:
         logging.info(f"{repo.organization.login}/{repo.name}")
-        createDirectory(repo.organization.login)
+        create_dir(repo.organization.login)
         get_projects(repo.organization.login, repo)
     else:
         logging.info(f"{g.get_user().login}/{repo.name}")
-        createDirectory(g.get_user().login)
+        create_dir(g.get_user().login)
         get_projects(g.get_user().login, repo)
